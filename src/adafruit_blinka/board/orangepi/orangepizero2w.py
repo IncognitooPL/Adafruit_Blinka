be22e436d5ee9266bbb437d51fab7b411597cb34
Marcin Sosnowski <sosnowskimarcinszizuw@gmail.com>
2025-01-12 18:46:21 +0100

    Added board to board.py

---
+++ src/adafruit_blinka/board/orangepi/orangepizero2w.py
@@ -0,0 +1,64 @@
+# SPDX-FileCopyrightText: 2021 Melissa LeBlanc-Williams for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+"""pin definitions for the Orange PI Zero 2W."""
+
+from adafruit_blinka.microcontroller.allwinner.h618 import pin
+
+PI1 = pin.PI1
+PI2 = pin.PI2
+PI3 = pin.PI3
+PI4 = pin.PI4
+PI5 = pin.PI5
+PI6 = pin.PI6
+PI7 = pin.PI7
+PI8 = pin.PI8
+PI9 = pin.PI9
+PI10 = pin.PI10
+PI11 = pin.PI11
+PI12 = pin.PI12
+PI13 = pin.PI13
+PI14 = pin.PI14
+PI15 = pin.PI15
+PI16 = pin.PI16
+
+PH0 = pin.PH0
+PH1 = pin.PH1
+PH2 = pin.PH2
+PH3 = pin.PH3
+PH4 = pin.PH4
+PH5 = pin.PH5
+PH6 = pin.PH6
+PH7 = pin.PH7
+PH8 = pin.PH8
+PH9 = pin.PH9
+
+PC12 = pin.PC12
+
+SDA0 = PI6
+SCL0 = PI5
+
+SDA1 = PI8
+SCL1 = PI7
+
+SCLK = PH6
+MOSI = PH7
+MISO = PH8
+SCK = SCLK
+
+CS0 = PH5
+CS1 = PH9
+
+PWM1 = PI11
+PWM2 = PI12
+
+UART3_TX = PI9
+UART3_RX = PI10
+
+UART4_TX = PI13
+UART4_RX = PI14
+
+UART5_TX = PH2
+UART5_RX = PH3
+
+
--- src/board.py
+++ src/board.py
@@ -122,6 +122,9 @@ elif board_id == ap_board.ORANGE_PI_PLUS_2E:
 elif board_id == ap_board.ORANGE_PI_2:
     from adafruit_blinka.board.orangepi.orangepipc import *
 
+elif board_id == ap_board.ORANGE_PI_ZERO_2W:
+    from adafruit_blinka.board.orangepi.orangepizero2w import *
+
 elif board_id == ap_board.ORANGE_PI_ZERO_PLUS_2H5:
     from adafruit_blinka.board.orangepi.orangepizeroplus2h5 import *
 
